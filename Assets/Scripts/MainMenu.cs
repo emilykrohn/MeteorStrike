ebd4c34e19596db225f43bc3c4e06ce727a07fa9
Emily Krohn <emilykrohn2@gmail.com>
2024-09-23 14:58:47 -0700

    Start Button Loads Game Scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Button startButton;
+    private Button quitButton;
+
+    private void OnEnable()
     {
-        
+        var UIDoc = GetComponent<UIDocument>();
+
+        startButton = UIDoc.rootVisualElement.Q("StartButton") as Button;
+        quitButton = UIDoc.rootVisualElement.Q("QuitButton") as Button;
+
+        startButton.RegisterCallback<ClickEvent>(StartGame);
+    }
+    
+    private void OnDisable()
+    {
+        startButton.UnregisterCallback<ClickEvent>(StartGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartGame(ClickEvent evt)
     {
-        
+        SceneManager.LoadScene("Game");
     }
 }
