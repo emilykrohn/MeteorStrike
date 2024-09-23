a7f1197ff464232749b709ab3fecccf2a6eb8110
Emily Krohn <emilykrohn2@gmail.com>
2024-09-22 17:13:12 -0700

    Add Player rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -28,5 +28,12 @@ public class PlayerMovement : MonoBehaviour
             position += moveDirection * moveSpeed * Time.deltaTime;
             transform.position = position;
         }
+
+        // https://discussions.unity.com/t/lookat-2d-equivalent/88118
+        Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        direction.Normalize();
+        float rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotation - 90);
+        
     }
 }
