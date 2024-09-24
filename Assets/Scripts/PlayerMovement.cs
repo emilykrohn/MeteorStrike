aed05f1425d09c6c972def6b1e162eef90faecc8
Emily Krohn <emilykrohn2@gmail.com>
2024-09-24 16:52:05 -0700

    Create Border that Blocks Player
    Player can't leave the screen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     // https://docs.unity3d.com/Packages/com.unity.inputsystem@1.7/manual/Actions.html#polling-actions
+    // CS386 Example02 InputExample.cs
     [SerializeField]
     PlayerInput playerInput;
     InputAction moveAction;
     [SerializeField]
     float moveSpeed = 10.0f;
     Vector2 position;
-
+    Vector2 moveDirection = Vector2.zero;
+    Rigidbody2D rb;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         position = transform.position;
         moveAction = playerInput.actions.FindAction("Movement");
         moveAction.Enable();
@@ -24,9 +27,9 @@ public class PlayerMovement : MonoBehaviour
     {
         if (playerInput.actions["Movement"].IsPressed())
         {
-            Vector2 moveDirection = moveAction.ReadValue<Vector2>();
-            position += moveDirection * moveSpeed * Time.deltaTime;
-            transform.position = position;
+            moveDirection = moveAction.ReadValue<Vector2>();
+        } else {
+            moveDirection = Vector2.zero;
         }
 
         // https://discussions.unity.com/t/lookat-2d-equivalent/88118
@@ -35,5 +38,10 @@ public class PlayerMovement : MonoBehaviour
         float rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rotation - 90);
         
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (moveDirection * moveSpeed * Time.deltaTime));
     }
 }
