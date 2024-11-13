b6a860a9fb0681753ea524b7ef7b853718ff0654
Emily Krohn <emilykrohn2@gmail.com>
2024-11-13 09:06:53 -0800

    Main Menu Button Works for Game Over Screen

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -11,22 +11,29 @@ using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour
 {
     [SerializeField] PlayerSaveData playerSaveData;
+
+    AudioSource buttonAudioSource;
     
     // UI Elements
     private Button restartButton;
+    private Button mainMenuButton;
     private Button quitButton;
 
     private void OnEnable()
     {
+        buttonAudioSource = GameObject.FindGameObjectWithTag("ButtonSound").GetComponent<AudioSource>();
+
         // Get where all of the UI elements are located
         var UIDoc = GetComponent<UIDocument>();
 
         // Find the Restart and Quit buttons in the UI Document as Buttons
         restartButton = UIDoc.rootVisualElement.Q("RestartButton") as Button;
+        mainMenuButton = UIDoc.rootVisualElement.Q("MainMenuButton") as Button;
         quitButton = UIDoc.rootVisualElement.Q("QuitButton") as Button;
 
         // When the buttons are pressed, call the RestartGame / QuitGame functions
         restartButton.RegisterCallback<ClickEvent>(RestartGame);
+        mainMenuButton.RegisterCallback<ClickEvent>(MainMenu);
         quitButton.RegisterCallback<ClickEvent>(QuitGame);
     }
     
@@ -37,12 +44,22 @@ public class GameOver : MonoBehaviour
         quitButton.UnregisterCallback<ClickEvent>(QuitGame);
     }
 
+    private void MainMenu(ClickEvent evt)
+    {
+        buttonAudioSource.volume = playerSaveData.sfxVolume / 100;
+        buttonAudioSource.Play();
+        // Loads Game Scene
+        SceneManager.LoadScene("MainMenu");
+    }
+
     /// <summary>
     /// Loads the Game Scene and sets the PlayerSaveData isLoadGame to false so the PlayerSaveData is loaded into the game
     /// </summary>
     /// <param name="evt"></param>
     private void RestartGame(ClickEvent evt)
     {
+        buttonAudioSource.volume = playerSaveData.sfxVolume / 100;
+        buttonAudioSource.Play();
         // Loads Game Scene
         SceneManager.LoadScene("Game");
         // If isLoadGame is false, then the GameUI Script Resets the Player stats
